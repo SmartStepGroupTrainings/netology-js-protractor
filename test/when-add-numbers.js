14f57fa9d75d6460bf4766cfa7c1e7c3826c92b4
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2016-10-13 23:45:25 +0300

    second test when-add-numbers return sum

---
+++ test/when-add-numbers.js
@@ -0,0 +1,14 @@
+describe('When add numbers', function() {
+    describe('super calculator should', function() {
+        it('return sum of 1 and 2', function() {
+            browser.get('http://juliemr.github.io/protractor-demo/');
+            element(by.model('first')).sendKeys(1);
+            element(by.model('second')).sendKeys(2);
+
+            element(by.id('gobutton')).click();
+
+            var sum = element(by.binding('latest')).getText();
+            expect(sum).toEqual((1 + 2).toString());
+        });
+    });
+});
